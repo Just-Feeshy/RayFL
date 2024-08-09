e63127227a529f6db1759a6ede1337b2709fb612
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-08-09 07:17:48 -0700

    Fullscreen

--- khafile.js
+++ khafile.js
@@ -12,15 +12,8 @@ project.addParameter('-dce full'); // Dead Code Elimination (Haxe, do your thing
 project.buildPath = BUILD_DIR;
 
 if(platform === Platform.HTML5) {
+    project.addDefine('kha_html5_disable_automatic_size_adjust');
     project.addDefine('js-es6');
-}
-function copy_callback(err) {
-    if (err) throw err;
-}
-
-const BUILD_INDEX = BUILD_DIR + '/html5/' + INDEX_HTML;
-if (platform == Platform.HTML5 && fs.existsSync(BUILD_INDEX)) {
-    fs.copyFile(INDEX_HTML, BUILD_INDEX, copy_callback);
 }
 
 resolve(project);
--- source/Main.hx
+++ source/Main.hx
@@ -6,12 +6,23 @@ import kha.Framebuffer;
 import kha.Scheduler;
 import kha.graphics4.*;
 
+#if kha_html5
+import js.Browser.document;
+import js.Browser.window;
+import js.html.CanvasElement;
+import kha.Macros;
+#end
+
 class Main {
     private static var FRAMERATE = 60;
 
     private var screen:Screen;
 
     private function new() {
+
+        trace("Width: " + System.windowWidth());
+        trace("Height: " + System.windowHeight());
+
         screen = new Screen();
     }
 
@@ -28,20 +39,12 @@ class Main {
     }
 
     public static function main() {
-        var width, height;
-
-        #if (js && html5)
-        width = untyped js.Syntax.code("window.innerWidth");
-        height = untyped js.Syntax.code("window.innerHeight");
-        #else
-        width = System.windowWidth();
-        height = System.windowHeight();
-        #end
+        setFullWindowCanvas();
 
         System.start({
             title: "Project",
-            width: width,
-            height: height,
+            width: 1280,
+            height: 720,
             framebuffer: {samplesPerPixel: 4}
         }, function(_) {
             Assets.loadEverything(function() {
@@ -55,4 +58,31 @@ class Main {
             });
         });
     }
+
+    private static function setFullWindowCanvas():Void {
+		#if kha_html5
+		document.documentElement.style.padding = "0";
+		document.documentElement.style.margin = "0";
+		document.body.style.padding = "0";
+		document.body.style.margin = "0";
+		final canvas:CanvasElement = cast document.getElementById(Macros.canvasId());
+		canvas.style.display = "block";
+		final resize = function() {
+			var w = document.documentElement.clientWidth;
+			var h = document.documentElement.clientHeight;
+			if (w == 0 || h == 0) {
+				w = window.innerWidth;
+				h = window.innerHeight;
+			}
+			canvas.width = Std.int(w * window.devicePixelRatio);
+			canvas.height = Std.int(h * window.devicePixelRatio);
+			if (canvas.style.width == "") {
+				canvas.style.width = "100%";
+				canvas.style.height = "100%";
+			}
+		}
+		window.onresize = resize;
+		resize();
+		#end
+	}
 }
