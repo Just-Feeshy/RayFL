e7baf83b98c48cb644ca7b2584ecd66c59c600cf
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-08-09 18:08:30 -0700

    gl_fragcoord

--- shaders/universe.frag.glsl
+++ shaders/universe.frag.glsl
@@ -5,5 +5,10 @@
 out vec4 fragColor;
 
 void main() {
+    if (gl_FragCoord.x > 100.0) {
+        fragColor = vec4(0.0, 1.0, 0.0, 1.0);
+        return;
+    }
+
     fragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
