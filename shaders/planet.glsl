49273c5573919125f4c6c65b983309bc62682461
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-09-11 13:46:12 -0400

    Earth Pt. 1

---
+++ shaders/planet.glsl
@@ -0,0 +1,154 @@
+
+#define EARTH_TILT 0.40840704496
+#define CLOSEST_DIST 100000000.0
+#define MAX_STEPS 80
+#define MIN_DIST 0.00001
+#define PI 3.14159265
+#define TAU (2*PI)
+
+// Raymarching with multiple planets
+
+struct ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+struct planet {
+    vec3 position;
+    float radius;
+};
+
+float atan2(in float y, in float x) {
+    return y > 0.0 ? atan(y, x) + PI : -atan(y, -x);
+}
+
+vec3 rotateY(vec3 p, float angle) {
+    float cosT = cos(angle);
+    float sinT = sin(angle);
+
+    return vec3(
+        p.x * cosT + p.z * sinT,
+        p.y,
+        p.z * cosT - p.x * sinT
+    );
+}
+
+vec3 rotateX(vec3 p, float angle) {
+    float cosT = cos(angle);
+    float sinT = sin(angle);
+
+    return vec3(
+        p.y * sinT + p.x * cosT,
+        p.y * cosT - p.x * sinT,
+        p.z
+    );
+}
+
+vec2 sphereUV(vec3 p, float t) {
+    p = rotateX(p, EARTH_TILT);
+    p = rotateY(p, t);
+
+    float phi = atan2(p.z, p.x);
+    return vec2(phi / TAU, acos(p.y) / PI);
+}
+
+float distancePlanet(vec3 p, float radius, sampler2D planetTexture, float rot) {
+    vec2 coordinates = sphereUV(normalize(p), rot);
+    coordinates.y = coordinates.y * 0.5 + 0.5;
+    vec4 elevation = texture(planetTexture, coordinates);
+
+    float f = length(p) - radius;
+
+    float elevationScale = radius * 0.0;
+    return f - length(elevation.rgb) * elevationScale;
+}
+
+void marchedSphere(ray r,
+        planet pl,
+        float rot,
+        out vec3 hitPos,
+        out vec3 nor,
+        out bool hit,
+        out float t,
+        sampler2D text) {
+    vec3 p = r.origin;
+    float closestDist = CLOSEST_DIST;
+    float minDist = MIN_DIST;
+    int steps = 0;
+    float dist = dot(p, p) + 1.0;
+    t = 0.0;
+
+    while(closestDist > minDist && steps++ <= MAX_STEPS) {
+        closestDist = distancePlanet(p, pl.radius, text, rot);
+        float cameraDist = dot(r.origin - p, r.origin - p);
+        minDist = MIN_DIST + clamp(cameraDist / 40, 0.0, 0.0075);
+
+        if(closestDist <= 0.0) {
+            break;
+        }
+
+        t += closestDist;
+        p += r.direction * closestDist;
+
+        if(dot(p, p) > dist * 1.25) {
+            hit = false;
+            return;
+        }
+    }
+
+    // Check if we hit the sphere
+    if(closestDist <= minDist + MIN_DIST) {
+        hitPos = p;
+        vec3 pp = p / pl.radius;
+        nor = normalize(p);
+
+        vec3 tangent = normalize(nor * vec3(1.0, 0.0, 1.0));
+        tangent.xz = vec2(tangent.z, -tangent.x);
+        vec3 bitangent = -cross(nor, tangent);
+
+        vec2 texCoord = sphereUV(nor, rot);
+        texCoord.y = texCoord.y * 0.5 + 0.5;
+
+        vec3 texSample = texture(text, texCoord).rgb * 2.0 - 1.0;
+        // nor = normalize(texSample.r * tangent + texSample.g * bitangent + texSample.b * nor);
+
+        hit = true;
+    }else {
+        hit = false;
+    }
+}
+
+vec2 raySphereIntersect(ray r, float radius, out bool hit) {
+    float a = 2.0 * dot(r.origin, r.direction);
+    float b = dot(r.origin, r.origin) - radius * radius;
+    float disc = a * a - 4.0 * b;
+
+    if(disc > 0.0) {
+        float t = sqrt(disc);
+        float near = (-a - t) * 0.5;
+        float far = (-a + t) * 0.5;
+
+        if(far >= 0.0) {
+            hit = true;
+            return vec2(near, far);
+        }
+    }
+
+    hit = false;
+    return vec2(0.0);
+}
+
+void drawPlanet(ray r, planet pl, sampler2D text, float rot, inout vec3 combinedColor) {
+    vec3 worldPos = r.origin - pl.position;
+    vec3 hitPos = vec3(0.0);
+    vec3 nor = vec3(0.0);
+    bool hit = false;
+    float t = 0.0;
+
+    marchedSphere(r, pl, rot, hitPos, nor, hit, t, text);
+
+    if(hit) {
+        // combinedColor = vec3(1.0);
+        combinedColor = texture(text, sphereUV(nor, rot)).rgb;
+    }
+}
--- shaders/universe.frag.glsl
+++ shaders/universe.frag.glsl
@@ -1,10 +1,13 @@
 #version 450
 
 #include "cursor.glsl"
+#include "planet.glsl"
 
-#define PI 3.14159265
-#define TAU (2*PI)
 #define MAX_TEXTURES 3
+#define EARTH_TILT 0.40840704496
+#define CLOSEST_DIST 100000000.0
+#define MAX_STEPS 80
+#define MIN_DIST 0.00001
 
 uniform float iTime;
 uniform vec3 iCam;
@@ -18,73 +21,13 @@ out vec4 fragColor;
 
 const float FOV = 1.0;
 
-struct ray {
-    vec3 origin;
-    vec3 direction;
-};
-
-float atan2(in float y, in float x) {
-    return y > 0.0 ? atan(y, x) + PI : -atan(y, -x);
-}
-
-vec3 rotateY(vec3 p, float angle) {
-    float cosT = cos(angle);
-    float sinT = sin(angle);
-
-    return vec3(
-        p.x * cosT + p.z * sinT,
-        p.y,
-        p.z * cosT - p.x * sinT
-    );
-}
-
-vec2 sphereUV(vec3 p, float t) {
-    p = rotateY(p, t);
-
-    float phi = atan2(p.z, p.x);
-    return vec2(phi / TAU, acos(p.y) / PI);
-}
-
-bool sphere(ray r, float radius, out float t) {
-    float a = dot(r.direction, r.direction);
-    float b = dot(r.origin, r.direction);
-    float c = dot(r.origin, r.origin) - radius * radius;
-    float disc = b * b - c * a;
-
-    if (disc > 0.0) {
-        t = (-b - sqrt(disc)) / a;
-    }else {
-        t = -b / a;
-    }
-
-    return disc > 0.0 && dot(r.direction, -r.origin) > 0.0;
-}
-
 vec3 render(vec2 uv) {
     vec3 ro = iCam - vec3(0.0, 0.0, 100.0);
     vec3 rd = normalize(normalize(vec3(uv, FOV)) * iMat);
 
     vec3 col = vec3(0.0);
-    float dist = 0.0;
 
-    bool hit_sphere = sphere(ray(ro, rd), 20.0, dist);
-
-    if (hit_sphere) {
-        vec3 p = ro + rd * dist;
-        vec3 nor = normalize(p);
-        vec3 ligthDir = vec3(3.0, 1.0, 0.0);
-        float light = dot(nor, normalize(ligthDir));
-        float normlight = max(0.0, (light + 1.0) * 0.5);
-        vec2 texCoord = sphereUV(nor, iTime * 0.1);
-        vec2 atmosphereCoord = sphereUV(nor, iTime * 0.15);
-
-        vec4 planet = texture(textures[0], texCoord.xy);
-        vec4 dark = texture(textures[1], texCoord.xy);
-        vec4 atmosphere = texture(textures[2], atmosphereCoord.xy);
-        float darkAverage = (dark.r + dark.g + dark.b) / 3.0;
-
-        col = (planet.rgb * normlight) + (atmosphere.rgb * atmosphere.a * 0.5 * min(1.0, normlight + darkAverage)) + ((1.0 - sqrt(normlight)) * dark.rgb);
-    }
+    drawPlanet(ray(ro, rd), planet(vec3(0.0), 40.0), textures[0], iTime * 0.2, col);
 
     return col;
 }
@@ -94,13 +37,16 @@ vec2 getUV(vec2 rayOffset) {
 }
 
 vec3 renderAAx4() {
-    vec4 offsets = vec4(0.125, -0.125, 0.375, -0.375);
-    vec3 colAA = render(getUV(offsets.xz)) + render(getUV(offsets.yw)) + render(getUV(offsets.wx)) + render(getUV(offsets.zy));
-    return colAA * 0.25;
+    //vec4 offsets = vec4(0.125, -0.125, 0.375, -0.375);
+    //vec3 colAA = render(getUV(offsets.xz)) + render(getUV(offsets.yw)) + render(getUV(offsets.wx)) + render(getUV(offsets.zy));
+    //return colAA * 0.25;
+    vec3 colAA = render(getUV(vec2(0.0)));
+    return colAA;
 }
 
 void main() {
     vec3 col = renderAAx4();
     col = cursor(getUV(vec2(0.0)), col);
+
     fragColor = vec4(col, 1.0);
 }
